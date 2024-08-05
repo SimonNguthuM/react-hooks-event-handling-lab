a45c9506460631cc5ad01f637845b27560a798bd
Simon Nguthu <simon.nguthu@student.moringaschool.com>
2024-08-05 15:08:52 +0300

    events

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,8 +1,14 @@
 // Code Keypad Component Here
+import React from "react";
 
 function Keypad (){
+    function changer() {
+        console.log('Entering password...');
+      }
     return (
-        <div></div>
+        <div>
+            <input id="changer" onChange={changer} type="password" />
+        </div>
     )
 }
 
